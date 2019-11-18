991bd1ae88045d4593010685f6528cd84616121e
Eddie <eddietseng@outlook.com>
2019-11-18 10:24:04 -0500

    Added redux folder structure to src

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Button} from './src/components/Button'
+import store from './src/store/index'
+import {Provider} from 'react-redux'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Button />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Button />
+      </View>
+    </Provider>
   );
 }
 
---
+++ FrontEnd/src/reducers/decklist.js
@@ -0,0 +1,3 @@
+const deckListReducer = (state =[] , action) => {
+  return state
+}
