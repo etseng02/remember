36e65a7336b7924dccc97e787b955f03720c50f1
Eddie <eddietseng@outlook.com>
2019-11-22 17:29:39 -0500

    Added redux persist with async storage

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { store, persistor } from './src/store/index'
+import { store, persistor } from './src/store'
 import {Provider} from 'react-redux'
 import MainNavigator from './src/config/routes'
 
@@ -8,8 +8,6 @@ import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/es/integration/react";
 
 export default function App() {
-
-  const persistor = persistStore(store);
  
   store.subscribe(()=>{
     console.log(store.getState())
--- FrontEnd/src/store/index.js
+++ FrontEnd/src/store/index.js
@@ -1,22 +1,20 @@
 import {createStore} from 'redux'
 import rootReducer from '../reducers/index'
 import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
-import AsyncStorage from '@react-native-community/async-storage';
-
+import { AsyncStorage } from 'react-native';
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
+  stateReconciler: autoMergeLevel2,
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-export default () => {
-  let store = createStore(persistedReducer)
-  let persistor = persistStore(store)
-  return { store, persistor }
-}
+export const store = createStore(persistedReducer)
+export const persistor = persistStore(store)
+ // return { store, persistor }
 
 
 //export default store = createStore(rootReducer)
