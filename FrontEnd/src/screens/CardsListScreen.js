7eb325d0ec5508940612db72018d141b13d2db53
Eddie <eddietseng@outlook.com>
2019-12-09 15:07:28 -0500

    Added background colour to cards list screen and cards edit screen

--- FrontEnd/src/screens/CardsListScreen.js
+++ FrontEnd/src/screens/CardsListScreen.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 import Logo from '../components/Logo'
 import CardsList from '../containers/CardsList'
+import EStyleSheet from 'react-native-extended-stylesheet'
 
 class CardsListScreen extends Component{
 
@@ -16,14 +17,9 @@ class CardsListScreen extends Component{
 
 export default CardsListScreen;
 
-const Styles = StyleSheet.create({
-  appTitle:{
-    fontSize: 28,
-    alignSelf: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-    marginTop: 25,
-  },
+const Styles = EStyleSheet.create({
   container:{
-  }
+    backgroundColor: '$primaryBackgroundDark',
+    flex: 1
+  },
 })
--- FrontEnd/src/screens/EditCardScreen.js
+++ FrontEnd/src/screens/EditCardScreen.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text} from 'react-native'
 import Logo from '../components/Logo'
 import EditCard from '../containers/EditCard'
+import EStyleSheet from 'react-native-extended-stylesheet'
 
 class EditCardScreen extends Component{
 
   render(){
     return(
       <View style={Styles.container}>
-        <Logo style={Styles.appTitle}/>
         <EditCard navigation={this.props.navigation}/>
       </View>
     )
@@ -17,14 +17,9 @@ class EditCardScreen extends Component{
 
 export default EditCardScreen;
 
-const Styles = StyleSheet.create({
-  appTitle:{
-    fontSize: 28,
-    alignSelf: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-    marginTop: 25,
-  },
+const Styles = EStyleSheet.create({
   container:{
-  }
+    backgroundColor: '$primaryBackgroundDark',
+    flex: 1
+  },
 })
